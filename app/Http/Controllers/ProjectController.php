8a97ceeef571e106d5a9a619f9a7da5e4a6a1582
Grossemain <grossemain@gmail.com>
2024-06-10 16:04:50 +0200

    MVC project OK

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $projects = Project::all();
+        return view('projects.create', compact('projects'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nameProject' => 'required',
+            'id_team' => 'required',
+        ]);
+
+        Project::create([
+            'nameProject' => $request->nameProject,
+            'id_team' => $request->id_team,
+        ]);
+
+        return redirect()->route('project.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $project = Project::findOrfail($id);
+        return view('projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        if (Auth::user()->id == $project->id) {
+            return view('projects.edit', compact('project'));
+        } else {
+            return redirect()->route('home')->withErrors(['erreur' => 'Vous n\'êtes pas autorisé à modifier ce projet']);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $updateProject = $request->validate([
+            'nameProject' => 'required',
+            'id_team' => 'required',
+        ]);
+        Project::whereId($id)->update($updateProject);
+        return redirect()->route('projects.index')
+            ->with('success', 'Le projet a ete mis à jour avec succès !');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $project = Project::findOrFail($id);
+        $project->delete();
+        return redirect('/projects')->with('success', 'Projet supprimé avec succès');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('projects', ProjectController::class);
+Route::get('/projects', [ProjectController::class, 'index'])->name('project');
