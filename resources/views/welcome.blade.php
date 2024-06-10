f2565eab402ad806602021f777b3dce2539e141b
ludolpr <ludolpr@gmail.com>
2024-06-10 14:47:34 +0200

    envoie n2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <link href="/public/bootstrap/css/bootstrap.css">
 
 </head>
 <h1>test</h1>
