5f17067925e2efbf9b43bd46e81cc55f12dd1419
Grossemain <grossemain@gmail.com>
2024-06-11 16:47:19 +0200

    push vues projets OK

--- database/seeders/UserTeamSeeder.php
+++ database/seeders/UserTeamSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ProjectSeeder extends Seeder
+class UserTeamSeeder extends Seeder
 {
     /**
      * Run the database seeds.
