b2191cf04ac9aec665b2997b48b10e3e1808f4a8
Nino-YM <119847747+Nino-YM@users.noreply.github.com>
2024-06-11 16:03:45 +0200

    ajout seeder user team

---
+++ database/seeders/UserTeamSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('user_team')->insert(
+            [
+                [
+                    'user_id' => '1',
+                    'team_id' => '1',
+                ],
+            ]
+
+        );
+    }
+}
